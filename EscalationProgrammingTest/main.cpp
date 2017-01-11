dbdfb730a1d4d0af3fa6986defc42729308ea3f7
zomawai.sailo <zomawai.sailo@SEA-8E40F1>
2017-01-11 12:33:56 -0800

    gah

--- EscalationProgrammingTest/main.cpp
+++ EscalationProgrammingTest/main.cpp
@@ -14,35 +14,43 @@ struct Entity {
 	char pad[12];
 };
 
+class Mob {
+public:
+	Entity monster;
+	void print() {
+
+	}
+};
+
 void main() {
 
 	std::fstream file;
 
 	file.open("mapfile.bin", std::ios_base::in | std::ios_base::binary);
 
-	unsigned size;
-	Entity *ents;
+	unsigned size;	
+	Entity *ents;		
+	std::list<Mob*> mobList;
+	Entity *ptr = new Entity;
 
 	if (file.is_open()) {		
 
 		file.read((char*)&size, sizeof(unsigned));
 		
 		ents = new Entity[size];
-		
+
 		while (!file.eof()) {
-			for (int i = 0; i < size; ++i) {
-				file.read((char*)ents, sizeof(Entity));
-				ents[i] = *ents;
-
-
+				ptr = file.read((char*)ents, sizeof(Entity));
+				
 				cout << ents->UniqueID << " "
 					<< ents->EntityID << " "
 					<< ents->posX << " "
 					<< ents->posY << endl;
-			}
 		}
 
 	}
 
 	file.close();
+
+	system("pause");
 }
